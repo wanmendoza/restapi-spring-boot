d8c2a90f79dba65a0856ea0199bd66bed9950a98
Wan Mendoza <wanmendoza@pws-mbp-01.local>
2018-05-10 08:53:56 -0600

    Add swagger an swagger-ui to documentation

--- src/main/java/com/wanmendoza/restapi/RestapiApplication.java
+++ src/main/java/com/wanmendoza/restapi/RestapiApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EntityScan
+@EnableSwagger2
 public class RestapiApplication {
 
 	public static void main(String[] args) {
